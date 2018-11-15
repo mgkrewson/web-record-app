4aaaba7570c0818aed93aede656badcde1f8ef43
mgkrewson <mgkrewson@gmail.com>
2018-11-15 13:00:32 -0600

    removed bower_components - no change

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const listenPort = process.env.PORT || 3000;
 const server = app.listen(listenPort, function () {
     // let host = server.address().address;
     let port = server.address().port;
-    console.log('Server running at http://%s:%s/', port);
+    console.log('Server running at http://%s:%s/', hostname, port);
 });
 
 
